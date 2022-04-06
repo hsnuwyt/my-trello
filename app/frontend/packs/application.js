d107dcee55e83ff3c4ec8693b41435ac8ea14e57
吳彥廷 <wyt@piplup.local>
2022-04-07 00:17:29 +0800

    first push

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'scripts'
+import 'styles'
 
 Rails.start()
 Turbolinks.start()
